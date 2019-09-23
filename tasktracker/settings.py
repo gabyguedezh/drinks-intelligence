7a09f30e05ce07462d62db5879ed4d209927bf16
gabyguedezh <gaby.guedez@gmail.com>
2019-09-23 20:49:47 +0100

    Added AWS config to settings file

--- tasktracker/settings.py
+++ tasktracker/settings.py
@@ -145,6 +145,20 @@ MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'tasktracker.storage_backends.MediaStorage'
 
+# AWS config
+
+AWS_S3_USER=config('AWS_S3_USER')
+AWS_ACCESS_KEY_ID=config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN=config('AWS_S3_CUSTOM_DOMAIN')
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION='media'
+
 # Messages
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
